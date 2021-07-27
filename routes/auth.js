08f9e650bfb2e179e25982dfa90d97db9e58d534
bonuk-gu <bigticket215@gmail.com>
2021-07-27 18:57:22 +0900

    ver1.0

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ module.exports = function(passport){
                 <input type="password" name="pwd" placeholder="password"></p>
             </p>
             <p>
-                <input type="submit" value="login">
+                <input type="submit" value="register">
             </p>
         </form>`, '');
         response.send(html);
@@ -25,6 +25,41 @@ module.exports = function(passport){
             req.session.save( () => { res.redirect('/') })
         }
     );
+
+    router.get('/register', function(request, response){ 
+        var title = 'login';
+        var list = template.list(request.list);
+        var html = template.html(title, list, `<form action="/auth/register_process" method="post">
+            <p><input type="text" name="email" placeholder="email"></p>
+            <p>
+                <input type="password" name="pwd" placeholder="password"></p>
+            <p>
+                <input type="password" name="pwd2" placeholder="password"></p>
+            </p>
+            <p><input type="text" name="displayName" placeholder="display name"></p>
+            <p>
+                <input type="submit" value="register">
+            </p>
+        </form>`, '');
+        response.send(html);
+    })
+
+    router.post('/register_process', function(request, response){
+        var post = request.body;
+        var email = post.email;
+        var pwd = post.pwd;
+        var pwd2 = post.pwd2;
+        var displayName = post.displayName;
+        var user = {
+            email: `${email}`,
+            pwd: `${pwd}`,
+            displayName: `${displayName}`
+        }
+        fs.writeFile(`users/${displayName}`, JSON.stringify(user), 'utf8', function(err){
+            if (err) console.error(err);
+            response.redirect(`/`);
+        })
+    })
     
     router.get('/logout', function(request, response){
         request.logout();
